1262ac2395874b6ace8979d99d21bb6f2de2a91f
Ciffelia <mc.prince.0203@gmail.com>
2022-10-09 01:59:33 +0900

    ci: deploymentディレクトリをreleaseのassetsに追加

--- devtools/.release-it.js
+++ devtools/.release-it.js
@@ -7,11 +7,18 @@ module.exports = {
   },
   github: {
     release: true,
-    releaseName: 'v${version}'
+    releaseName: 'v${version}',
+    assets: ['./koe_*.zip']
   },
   plugins: {
     '@release-it/conventional-changelog': {
       preset: 'conventionalcommits'
     }
+  },
+  hooks: {
+    'after:bump':
+      "sed -i 's/${latestVersion}/${version}/g' ../deployment/docker-compose.yml",
+    'before:github:release':
+      "cp -r ../deployment ./koe && zip -r 'koe_${version}.zip' ./koe && rm -rf ./koe"
   }
 }
