3d990535b8bc474cd63a8a21b5add81aaa1b6c8c
Ciffelia <mc.prince.0203@gmail.com>
2022-10-09 22:53:22 +0900

    style(devtools): run prettier

--- devtools/.release-it.js
+++ devtools/.release-it.js
@@ -1,25 +1,25 @@
 module.exports = {
   git: {
-    commitMessage: 'chore: release v${version}'
+    commitMessage: 'chore: release v${version}',
   },
   npm: {
-    publish: false
+    publish: false,
   },
   github: {
     release: true,
     releaseName: 'v${version}',
-    assets: ['./koe_*.zip']
+    assets: ['./koe_*.zip'],
   },
   plugins: {
     '@release-it/conventional-changelog': {
-      preset: 'conventionalcommits'
-    }
+      preset: 'conventionalcommits',
+    },
   },
   hooks: {
     'after:bump':
       "sed -i 's/${latestVersion}/${version}/g' ../deployment/docker-compose.yml",
     'before:git:release': 'git add ../deployment/docker-compose.yml',
     'before:github:release':
-      "cp -r ../deployment ./koe && zip -r 'koe_${version}.zip' ./koe && rm -rf ./koe"
-  }
-}
+      "cp -r ../deployment ./koe && zip -r 'koe_${version}.zip' ./koe && rm -rf ./koe",
+  },
+};
--- devtools/src/generateDockerTags.js
+++ devtools/src/generateDockerTags.js
@@ -1,37 +1,37 @@
-const semver = require('semver')
-const pkg = require('../package.json')
+const semver = require('semver');
+const pkg = require('../package.json');
 
 const main = () => {
-  const { version } = pkg
+  const { version } = pkg;
 
-  const parsed = semver.parse(version)
+  const parsed = semver.parse(version);
   if (parsed === null) {
-    console.error(`version ${version} is invalid`)
-    process.exit(1)
+    console.error(`version ${version} is invalid`);
+    process.exit(1);
   }
 
-  console.log(generateTags(parsed).join(' '))
-}
+  console.log(generateTags(parsed).join(' '));
+};
 
 const generateTags = (v) => {
   if (v.prerelease.length !== 0) {
-    return [v.version]
+    return [v.version];
   }
 
   if (v.major === 0) {
     return [
       `${v.major}.${v.minor}.${v.patch}`,
       `${v.major}.${v.minor}`,
-      'latest'
-    ]
+      'latest',
+    ];
   }
 
   return [
     `${v.major}.${v.minor}.${v.patch}`,
     `${v.major}.${v.minor}`,
     `${v.major}`,
-    'latest'
-  ]
-}
+    'latest',
+  ];
+};
 
-main()
+main();
