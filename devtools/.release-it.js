d4c38256bf1751fed3d7a2580532d498daabf93b
Ciffelia <mc.prince.0203@gmail.com>
2022-10-10 02:13:40 +0900

    ci(release): `docker-compose.yml`の意図しない箇所を書き換えないよう修正

--- devtools/.release-it.js
+++ devtools/.release-it.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   hooks: {
     'after:bump':
-      "sed -i 's/${latestVersion}/${version}/g' ../deployment/docker-compose.yml",
+      "sed -i 's/koe:${latestVersion}/koe:${version}/g' ../deployment/docker-compose.yml",
     'before:git:release': 'git add ../deployment/docker-compose.yml',
     'before:github:release':
       "cp -r ../deployment ./koe && zip -r 'koe_${version}.zip' ./koe && rm -rf ./koe",
