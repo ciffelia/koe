dadce74c5cf23009b2312bc534cdf65090871130
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-11-08 15:00:42 +0000

    サーバーニックネームが未設定のとき、ユーザー名の代わりに表示名を読み上げるよう修正 (fixes #127)

--- crates/koe/src/message/read.rs
+++ crates/koe/src/message/read.rs
@@ -56,9 +56,7 @@ fn should_read_author_name(msg: &Message, last_msg: Option<&Message>) -> bool {
 async fn build_author_name(ctx: &Context, msg: &Message) -> String {
     msg.author_nick(&ctx.http)
         .await
-        // FIXME: `User::name`はユーザーの表示名ではなく一意のユーザー名を返す。現在のSerenityの実装では、ユーザーの表示名を取得する方法がない。
-        // cf. https://github.com/serenity-rs/serenity/discussions/2500
-        .unwrap_or_else(|| msg.author.name.clone())
+        .unwrap_or_else(|| msg.author.display_name().to_string())
 }
 
 /// [Message]の内容を返す。ID表記されたメンションやチャンネル名は読める形に書き換える。
