f41cf997bf8816a4a4c3096578f5a1b890d43bd1
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-11-09 09:05:35 +0000

    dev container: move installation commands from on-create.sh to Dockerfile

--- .devcontainer/scripts/on-create.sh
+++ .devcontainer/scripts/on-create.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-rustup toolchain install nightly --component rustfmt
-
-curl -fsSL https://claude.ai/install.sh | bash
-
 # Workaround for Claude Code IDE integration
 ln -s "$CONTAINER_WORKSPACE_FOLDER/.devcontainer/persist/.claude" "$HOME/.claude"
 
