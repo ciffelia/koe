1476a4276fb42b2af09c4db934cca10ca51410c8
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-11-09 06:21:27 +0000

    Add claude code to Dev Container

--- .devcontainer/scripts/on-create.sh
+++ .devcontainer/scripts/on-create.sh
@@ -2,3 +2,8 @@
 set -euxo pipefail
 
 rustup toolchain install nightly --component rustfmt
+
+curl -fsSL https://claude.ai/install.sh | bash
+
+# Workaround for Claude Code IDE integration
+ln -s "$CONTAINER_WORKSPACE_FOLDER/.devcontainer/persist/.claude" "$HOME/.claude"
