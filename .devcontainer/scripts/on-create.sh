413e4cd4724e231e8bc5dccfc4f4204ad98cfedd
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-11-16 06:34:47 +0000

    Fix dev container setup script

--- .devcontainer/scripts/on-create.sh
+++ .devcontainer/scripts/on-create.sh
@@ -4,6 +4,9 @@ set -euxo pipefail
 # Workaround for Claude Code IDE integration
 ln -s "$CONTAINER_WORKSPACE_FOLDER/.devcontainer/persist/.claude" "$HOME/.claude"
 
+# Install rust toolchain
+rustup toolchain install
+
 # Setup mise
 mise trust
 mise install
